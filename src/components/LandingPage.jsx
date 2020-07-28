734b4552acaaa8522e8f9d1a948dbf3689e1f78c
Shelcia abi <shelciaabi@Shelcias-MacBook-Air-2320.local>
2020-07-29 02:19:59 +0530

    update

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <React.Fragment>
+      <div className="landingpage">
+        <div className="overlay">
+          <h2>Team AUV</h2>
+          <br />
+          <h2>IIITDM Kancheepuram</h2>
+          <a href="#about">
+            <button type="button">Explore</button>
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default LandingPage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-container">
+      <div className="nav-logo">
+        <img src={Logo} height="40px" alt="logo"></img>
+      </div>
+      <div className="nav-items">
+        <a href="#about">About</a>
+        <a href="#acheivement">Acheivments</a>
+        <a href="#sponsors">Sponsors</a>
+        <a href="#post">Post</a>
+        <a href="#team">Team</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
