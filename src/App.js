3f84b9c90043e5d2e6856624bfedd061fdaf63e4
Shelcia abi <shelciaabi@Shelcias-MacBook-Air-2320.local>
2020-07-30 01:13:37 +0530

    mobile view responsiveness for about page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/style.css";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/About";
@@ -7,12 +7,24 @@ import Sponsors from "./components/Sponsors";
 import "animate.css/animate.min.css";
 
 const App = () => {
+  const [view, setView] = useState("instagram");
   return (
     <React.Fragment>
       <LandingPage />
       <AboutPage />
       <AchievementsPage />
       <Sponsors />
+      <div className="posts">
+        {view === "instgram" && (
+          <div className="instagram">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+        {view === "youtube" && <div className="youtube"></div>}
+        {view === "gallery" && <div className="gallery"></div>}
+      </div>
     </React.Fragment>
   );
 };
