d6d344cbb35554299608f76d21f082293977dbba
noob <shelcia1999@gmail.com>
2020-08-30 00:35:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/style.css";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/About";
@@ -7,20 +7,39 @@ import Sponsors from "./components/Sponsors";
 import Posts from "./components/Posts/Posts";
 import Teams from "./components/Teams/Teams";
 import Contact from "./components/Contact";
+import Loader from "react-loader-spinner";
 import "animate.css/animate.min.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Logo from "./assets/new_logo.png";
+
+const Loading = () => (
+  <div className="loading">
+    <img src={Logo} alt="logo" width="150px" />
+    <Loader type="MutatingDots" color="#0dc1f7" height={100} width={100} />
+  </div>
+);
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    let isLoading = null;
+    isLoading = setTimeout(() => setLoading(false), 5000);
+    return () => clearInterval(isLoading);
+  });
   return (
     <React.Fragment>
-      <div data-spy="scroll" data-target=".navbar" data-offset="50">
-        <LandingPage />
-        <AboutPage />
-        <AchievementsPage />
-        <Sponsors />
-        <Posts />
-        <Teams />
-        <Contact />
-      </div>
+      {loading && <Loading />}
+      {!loading && (
+        <React.Fragment>
+          <LandingPage />
+          <AboutPage />
+          <AchievementsPage />
+          <Sponsors />
+          <Posts />
+          <Teams />
+          <Contact />
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 };
--- src/components/Posts/Instagram.jsx
+++ src/components/Posts/Instagram.jsx
@@ -1,35 +1,56 @@
-import React from "react";
-// import InstagramEmbed from "react-instagram-embed";
+import React, { useState } from "react";
+import InstagramEmbed from "react-instagram-embed";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Instagram = ({ setView }) => {
   // const links = useState([
-  //   { id: 1, url: "CDOzSQtAqRz/" },
-  //   { id: 2, url: "CCq--regz9o/" },
-  //   { id: 3, url: "CClmUCyg1c8/" },
+  //   { id: 1, url: "CD8uHoZg56y" },
+  //   // { id: 2, url: "CCq--regz9o/" },
+  //   // { id: 3, url: "CClmUCyg1c8/" },
   // ]);
   return (
     <React.Fragment>
-      <div className="nav">
-        <p className="active">instagram</p>
-        <p onClick={() => setView("youtube")}>Youtube</p>
-        <p onClick={() => setView("gallery")}>Gallery</p>
+      <div className="col-sm-3">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <ul className="list-group">
+            <li className="list-group-item  active">Instagram</li>
+            <li className="list-group-item" onClick={() => setView("youtube")}>
+              Youtube
+            </li>
+            <li className="list-group-item" onClick={() => setView("gallery")}>
+              Gallery
+            </li>
+          </ul>
+        </ScrollAnimation>
       </div>
-      {/* {links.map((link) => (
-        <InstagramEmbed
-          url={`https://instagr.am/p/Zw9o4/`}
-          maxWidth={320}
-          hideCaption={false}
-          containerTagName="div"
-          protocol=""
-          injectScript
-          onLoading={() => {}}
-          onSuccess={() => {}}
-          onAfterRender={() => {}}
-          onFailure={() => {}}
-        />
-      ))} */}
-      <div className="powr-instagram-feed" id="dd429f15_1596699972"></div>
-      <script src="https://www.powr.io/powr.js?platform=react"></script>
+      <div className="col-sm-9 flow-control">
+        <div className="instagram-feed">
+          <InstagramEmbed
+            url={`https://instagr.am/p/CD8uHoZg56y/`}
+            maxWidth={320}
+            hideCaption={true}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+          <InstagramEmbed
+            url={`https://instagr.am/p/CD8uHoZg56y/`}
+            maxWidth={320}
+            hideCaption={true}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
