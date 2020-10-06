eb9fd689cb91d84b2d8e45a08f416c83dda49280
noob <shelcia1999@gmail.com>
2020-10-06 22:53:17 +0530

    Updates

---
+++ precache-manifest.fd01c982cd1b850e3fd814406fd1ef87.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27112fbb6d03c1b00c2fe4812405c7de",
+    "url": "/AUV/index.html"
+  },
+  {
+    "revision": "f8527cb672db9c526f35",
+    "url": "/AUV/static/css/2.ef2605f5.chunk.css"
+  },
+  {
+    "revision": "120ca5cbec091c3b168a",
+    "url": "/AUV/static/css/main.b57228cc.chunk.css"
+  },
+  {
+    "revision": "f8527cb672db9c526f35",
+    "url": "/AUV/static/js/2.d67871e8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AUV/static/js/2.d67871e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120ca5cbec091c3b168a",
+    "url": "/AUV/static/js/main.0404f7e3.chunk.js"
+  },
+  {
+    "revision": "798e940ec10ec250e501",
+    "url": "/AUV/static/js/runtime-main.15948625.js"
+  },
+  {
+    "revision": "364c2686fd2e80c3a56cf70764a77ffb",
+    "url": "/AUV/static/media/0 (1).364c2686.jpeg"
+  },
+  {
+    "revision": "586e7ef16b20d8d8ca764736f4259b49",
+    "url": "/AUV/static/media/0 (1).586e7ef1.jpeg"
+  },
+  {
+    "revision": "ecfb045d156a0cdd3b944f227bf7e787",
+    "url": "/AUV/static/media/0 (1).ecfb045d.jpeg"
+  },
+  {
+    "revision": "052a01ae255fd8dabe3803827ee0d944",
+    "url": "/AUV/static/media/0.052a01ae.jpeg"
+  },
+  {
+    "revision": "0d98b2378f273ff8ea7c84c4955c31f8",
+    "url": "/AUV/static/media/0.0d98b237.jpeg"
+  },
+  {
+    "revision": "2b9a32e856c66df8869adf7150b85625",
+    "url": "/AUV/static/media/0.2b9a32e8.jpeg"
+  },
+  {
+    "revision": "531c70a27122c8fbf1d43a194e0a7750",
+    "url": "/AUV/static/media/0.531c70a2.jpeg"
+  },
+  {
+    "revision": "bd678df7951db697cf8b861057cdb4bd",
+    "url": "/AUV/static/media/0.bd678df7.jpeg"
+  },
+  {
+    "revision": "d24679e911a60aa6b606653e386370a8",
+    "url": "/AUV/static/media/0.d24679e9.jpeg"
+  },
+  {
+    "revision": "0b17bde9f0556e4556db43cc8b7019e4",
+    "url": "/AUV/static/media/1.0b17bde9.JPG"
+  },
+  {
+    "revision": "9d11a6156156bd7d14750394d39424b6",
+    "url": "/AUV/static/media/1.9d11a615.PNG"
+  },
+  {
+    "revision": "21499f9e11ae3687ad2234d068753ddd",
+    "url": "/AUV/static/media/10.21499f9e.JPG"
+  },
+  {
+    "revision": "bd8d7a5d0f56304ece6bec0b6578b4d3",
+    "url": "/AUV/static/media/11.bd8d7a5d.JPG"
+  },
+  {
+    "revision": "7403bbb1a0930420ee1e75a1a9e81dfb",
+    "url": "/AUV/static/media/12.7403bbb1.JPG"
+  },
+  {
+    "revision": "41895ae667d6e7119b4379d60407a551",
+    "url": "/AUV/static/media/13.41895ae6.JPG"
+  },
+  {
+    "revision": "b14ba15e0bec33049105185a66d99d1c",
+    "url": "/AUV/static/media/2.b14ba15e.JPG"
+  },
+  {
+    "revision": "650cb3b636c8bd250ea944497a391e41",
+    "url": "/AUV/static/media/3.650cb3b6.JPG"
+  },
+  {
+    "revision": "e55b077925e76b4c9b19495d88664f26",
+    "url": "/AUV/static/media/4.e55b0779.JPG"
+  },
+  {
+    "revision": "f66d8f684f95ac13bffdbc3040d76d90",
+    "url": "/AUV/static/media/5.f66d8f68.JPG"
+  },
+  {
+    "revision": "3d8d10d48fb5e07e644b9010e1bce160",
+    "url": "/AUV/static/media/6.3d8d10d4.JPG"
+  },
+  {
+    "revision": "d79e11659b96b504ded58e673427fd47",
+    "url": "/AUV/static/media/7.d79e1165.JPG"
+  },
+  {
+    "revision": "02c0aae96f4bff6fa857235938872788",
+    "url": "/AUV/static/media/8.02c0aae9.JPG"
+  },
+  {
+    "revision": "e8761978a3d4838642b68ba9d492ad98",
+    "url": "/AUV/static/media/9.e8761978.JPG"
+  },
+  {
+    "revision": "de88674737653706dbe8b9373db6a0bf",
+    "url": "/AUV/static/media/AUV_v1.0_report.de886747.pdf"
+  },
+  {
+    "revision": "344db99cd30edc33a12b2644b43d0f33",
+    "url": "/AUV/static/media/DRDO_RIC_Logo-min.344db99c.png"
+  },
+  {
+    "revision": "1007f6be645e1bd4203fc7e87c26b29f",
+    "url": "/AUV/static/media/Image.1007f6be.jpg"
+  },
+  {
+    "revision": "aba11324a3ff222afd30478342aa8274",
+    "url": "/AUV/static/media/ThilakDP.aba11324.jpg"
+  },
+  {
+    "revision": "916afeab2bfb59462015c2f088ff5736",
+    "url": "/AUV/static/media/anupriya.916afeab.jpeg"
+  },
+  {
+    "revision": "5b1513274bff2de9bb9cd5e81aeb9375",
+    "url": "/AUV/static/media/arjun.5b151327.jpg"
+  },
+  {
+    "revision": "cb00fc2caea7c217e0e4cb26c4ac386f",
+    "url": "/AUV/static/media/bg.cb00fc2c.png"
+  },
+  {
+    "revision": "29ea90cf1b1553659a02fe59a5d94ca5",
+    "url": "/AUV/static/media/charan.29ea90cf.jpg"
+  },
+  {
+    "revision": "a078b72cd59c936bd911f6d85b069936",
+    "url": "/AUV/static/media/chit.a078b72c.jpg"
+  },
+  {
+    "revision": "b455c91c985e7b7f9e29366dd50a855e",
+    "url": "/AUV/static/media/daware.b455c91c.jpg"
+  },
+  {
+    "revision": "b4e429daf539f1ca91a1e93df4443490",
+    "url": "/AUV/static/media/face.b4e429da.svg"
+  },
+  {
+    "revision": "557dfd5a9f52fe826bcb0bdcb34cdd4c",
+    "url": "/AUV/static/media/github.557dfd5a.svg"
+  },
+  {
+    "revision": "082b33a710eeea5fb2bc91b4883de0c7",
+    "url": "/AUV/static/media/govind.082b33a7.png"
+  },
+  {
+    "revision": "80583924641592c36312f1babad80b0a",
+    "url": "/AUV/static/media/idea.80583924.png"
+  },
+  {
+    "revision": "02b43149c90acb1032346cc1b7738661",
+    "url": "/AUV/static/media/mayank.02b43149.jpg"
+  },
+  {
+    "revision": "19488022aabb953cdd24f05fc4a816b9",
+    "url": "/AUV/static/media/medal.19488022.svg"
+  },
+  {
+    "revision": "d51deb2ce49c80b25c1a4eb1f55a313f",
+    "url": "/AUV/static/media/new_logo.d51deb2c.png"
+  },
+  {
+    "revision": "b2cc74e192e45dccd32d5012de903206",
+    "url": "/AUV/static/media/prathamesh.b2cc74e1.jpg"
+  },
+  {
+    "revision": "5fed7ed32c43cf108114facafd081c57",
+    "url": "/AUV/static/media/ram.5fed7ed3.jpg"
+  },
+  {
+    "revision": "0ad22ad369bbe0fc2ccc04c16e5c16ea",
+    "url": "/AUV/static/media/ravi.0ad22ad3.jpg"
+  },
+  {
+    "revision": "036e5287cdf6b0344edfa353471910d4",
+    "url": "/AUV/static/media/roshan.036e5287.jpg"
+  },
+  {
+    "revision": "0214485546650cffa8eb2b4c83fdd157",
+    "url": "/AUV/static/media/sandesh.02144855.jpg"
+  },
+  {
+    "revision": "bf00be130d403be829a8b33bb05e934c",
+    "url": "/AUV/static/media/sauvclogo.bf00be13.png"
+  },
+  {
+    "revision": "0d98b2378f273ff8ea7c84c4955c31f8",
+    "url": "/AUV/static/media/sharath.0d98b237.jpeg"
+  },
+  {
+    "revision": "37fab0cd3c32a4700f0c36f8d9de84a4",
+    "url": "/AUV/static/media/shramona.37fab0cd.jpg"
+  },
+  {
+    "revision": "c591e34de29829fdc4aa7cac47cef2c1",
+    "url": "/AUV/static/media/subash-min.c591e34d.jpg"
+  },
+  {
+    "revision": "08f64b10af2bddd85d2b3ebdd77063e1",
+    "url": "/AUV/static/media/sukesh.08f64b10.jpeg"
+  },
+  {
+    "revision": "8b749c31b13e349d1df32f640c63f558",
+    "url": "/AUV/static/media/trophy.8b749c31.svg"
+  },
+  {
+    "revision": "4526099c98dc85f285fd043a222d853b",
+    "url": "/AUV/static/media/vaishali.4526099c.jpg"
+  },
+  {
+    "revision": "c41ebe5c44a9943a9829b3ebeaeb0e23",
+    "url": "/AUV/static/media/vishva.c41ebe5c.jpg"
+  }
+]);
